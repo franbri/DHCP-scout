8ae197ed3337216baef01e28727d569cb630e1bc
Francisco Briceno A. <francisco.briceno.a@mail.pucv.cl>
2024-07-21 00:29:26 -0400

    now reader uses fileman functions

--- reader.py
+++ reader.py
@@ -1,27 +1,5 @@
-import pickle,os
-from pathlib import Path
-
-if os.name == 'nt':
-    tmpDir = '.\\'
-elif os.name == 'posix':
-    try:
-        tmpDir = os.environ['TMPDIR']
-    except:
-        try:
-            Path('/dev/shm/test').touch()
-            tmpDir = '/dev/shm/'
-        except:
-            tmpDir = '/tmp/'
-
-def saveState(known_hosts):
-    stateFile = open(tmpDir + 'DHCP-scout.pickle', 'ab')
-    pickle.dump(known_hosts, stateFile)
-    stateFile.close()
-
-def loadState():
-    stateFile = open(tmpDir + 'DHCP-scout.pickle', 'rb')
-    return pickle.load(stateFile)
+import fileMan
 
 if __name__ == '__main__':
-    db = loadState()
+    db = fileMan.loadState()
     print(db)
