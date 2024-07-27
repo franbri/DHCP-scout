5b83e40ecd9a1a40eeda2d6b03ae8cd785a81d66
Francisco Briceno A. <francisco.briceno.a@mail.pucv.cl>
2024-07-26 23:04:00 -0400

    changed logic to use async sniffer to call the function asynchronously to try to catch all packets

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,9 @@
 import os
 import argparse
 import fileMan
+import time
 
-from scapy.all import sniff, conf, get_if_hwaddr, get_if_list, get_if_addr
+from scapy.all import sniff, conf, get_if_hwaddr, get_if_list, get_if_addr, AsyncSniffer
 
 if os.name == 'nt':
     from scapy.all import get_windows_if_list as _get_if_list
@@ -14,10 +15,45 @@ from scapy import layers
 from common import Host, PACKETS_WITHOUT_DHCP, get_requested_address
 from utils import get_time, check_port_open
 
+
+def packetHandler(packet):
+    try:
+        source_mac = packet.getlayer(layers.l2.Ether).src
+        dest_mac = packet.getlayer(layers.l2.Ether).dst
+    except:
+        return
+
+    ip_layer = packet.getlayer(layers.inet.IP)
+
+    if not ip_layer:
+        return
+
+    source_ip = ip_layer.src
+    dest_ip = ip_layer.dst
+
+    host = Host(source_mac)
+    host.set_ip(source_ip)
+
+    # more unique identifier
+    host = known_hosts.setdefault(host.mac, host)
+    host.increase_packet_num()
+
+    dhcp_layer = packet.getlayer(layers.dhcp.DHCP)
+
+    if dhcp_layer:
+        try:
+            requested_address = get_requested_address(dhcp_layer)
+        except ValueError:
+            print('Caught non-DCHPREQUEST packet')
+            return
+
+        host.set_ip(requested_address)
+        host.set_dhcp_seen()
+
+    #now = get_time()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--detail', action='store_true',
-                        help='show all packets to specified iface')
     parser.add_argument('-i', '--interface',
                         help='specify interface to work with')
     parser.add_argument('--show-interfaces', action='store_true',
@@ -27,86 +63,22 @@ if __name__ == '__main__':
     interface = args.interface or conf.iface
     interface_hwaddr = get_if_hwaddr(interface)
 
-    known_hosts = {}
+    try:
+        known_hosts = fileMan.loadState()
+    except:
+        known_hosts = {}
 
-    count = 0
-
-    if args.show_interfaces:
-        print('{:20} {:20} {:20}'.format('NAME', 'MAC', 'IP'))
-        if os.name == 'posix':
-            for iface in _get_if_list():
-                print('{:20} {:20} {:20}'.format(iface, get_if_hwaddr(iface),
-                                                 get_if_addr(iface)))
-        elif os.name == 'nt':
-            for iface in _get_if_list():
-                name = iface.get('name')
-                print('{:20} {:20}'.format(name, iface.get('mac')))
-        exit(0)
-
-    if args.detail:
-        print('Capturing all packets to interface {}'.format(interface))
-    else:
-        print('{:10} {:30} {:20} {:20}'.format('Type', 'Time', 'MAC', 'IP'))
-
+    sniffer = AsyncSniffer( iface=args.interface,
+                    filter="net 192.168.1.0/24 or (port 67 or 68)",
+                    lfilter=lambda d: d.src != interface_hwaddr,
+                    prn=packetHandler)
     while True:
-        packet = sniff(count=1, iface=args.interface,
-                       lfilter=lambda d: d.src != interface_hwaddr)[0]
-        try:
-            source_mac = packet.getlayer(layers.l2.Ether).src
-            dest_mac = packet.getlayer(layers.l2.Ether).dst
-        except:
-            continue
-
-        ip_layer = packet.getlayer(layers.inet.IP)
-
-        if not ip_layer:
-            continue
-
-        source_ip = ip_layer.src
-        dest_ip = ip_layer.dst
-
-        host = Host(source_mac)
-        host.set_ip(source_ip)
-
-        # more unique identifier
-        host = known_hosts.setdefault(host.mac + host.ip, host)
-        host.increase_packet_num()
-
-        if count > 1000:
-            fileMan.saveState(known_hosts)
-            count = 0
-        else:
-            count = count + 1
-
-        dhcp_layer = packet.getlayer(layers.dhcp.DHCP)
-
-        if dhcp_layer:
-            try:
-                requested_address = get_requested_address(dhcp_layer)
-            except ValueError:
-                print('Caught non-DCHPREQUEST packet')
-                continue
-
-            host.set_ip(requested_address)
-            host.set_dhcp_seen()
-
-        now = get_time()
-
-        if args.detail:
-            print(packet.summary())
-        else:
-            if host.num_packets > PACKETS_WITHOUT_DHCP and (
-                    not host.broadcasted_dhcp and not host.seen):
-                print('STATIC {:30} {:20} {:20}'.format(
-                    now, host.mac, host.ip
-                ))
-                host.set_seen()
-                if check_port_open(host.ip, 80):
-                    print('Can open browser at http://{}'.format(host.ip))
-
-            if host.broadcasted_dhcp and not host.seen:
-                print(
-                    'DYNAMIC {:30} {:20} {:20}'.format(now, host.mac, host.ip))
-                host.set_seen()
-                if check_port_open(host.ip, 80):
-                    print('Can open browser at http://{}'.format(host.ip))
+        '''sniff(count=1000, iface=args.interface,
+                       filter="net 192.168.1.0/24 or (port 67 or 68)",
+                       lfilter=lambda d: d.src != interface_hwaddr,
+                       prn=packetHandler)
+                       '''
+        sniffer.start()
+        time.sleep(10)
+        sniffer.stop()
+        fileMan.saveState(known_hosts)
