a9b9c817d0db546730991d445886a8a102027ddd
Francisco Briceño A. <francisco.briceno.a@mail.pucv.cl>
2024-07-27 04:47:21 -0400

    changed logic to fix if statement

--- sniffer.py
+++ sniffer.py
@@ -86,5 +86,5 @@ if __name__ == '__main__':
         dhcpFile = open('dhcp.leases', 'wb')
 
         for entry in temp_dict:
-            if entry.broadcasted_dhcp:
+            if entry.broadcasted_dhcp == True:
                 dhcpFile.write(entry.to_lease())
